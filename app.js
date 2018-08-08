075f3a17b6bc15cc9cda312fb9164cc792899157
yousuf451 <yousufshaikh92@gmail.com>
2018-08-08 16:49:43 +0500

    bootcamp-derver-first class

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express')  // importing express from express
+const app = express() // use express object in app variable
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))  // get root file 'localhost:3000/'
 
-app.get('/index.html', (req, res) => res.send('This is index.html page'))
+app.get('/test', (req, res) => res.send('This is test page')) // get test link 'localhost:3000/test'
 
 // app.put('/hello-one', (req, res) => res.send('Hello World Testing Two!'))
 
